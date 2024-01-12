19c84566bc72d37cf572470f6377f14e1bab6889
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-12 11:30:18 +0000

    Import comment from models & add definitions with returns to views for user being to be able to view comment

--- poems/urls.py
+++ poems/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.view_poems, name="poems"),
-    path("", views.post_detail, name="post_detail"),
     path("/edit_comment/<int:comment_id>",
          views.comment_edit, name='comment_edit'),
     path("/delete_comment/<int:comment_id>",
--- poems/views.py
+++ poems/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Poem
+from .models import Poem, Comment
 
 # Create your views here.
 
@@ -10,3 +10,69 @@ def view_poems(request):
         "poems": poems,
     }
     return render(request, template, context)
+
+def poem_detail(request):
+    queryset = Post.objects.filter(status=1)
+    poem = get_object_or_404(queryset)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
+
+    return render(
+        request,
+        "poems/poems.html",
+        {
+            "poem": poem,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
+        },
+    )
+
+
+def comment_edit(request, slug, comment_id):
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('poem_detail', args=[slug]))
+
+
+def comment_delete(request, comment_id):
+    queryset = Post.objects.filter(status=1)
+    poem = get_object_or_404(queryset)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('poem_detail', args=[slug]))

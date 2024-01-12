9a3c34f2a1b8efb24e176442d1246456da66f396
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-12 11:32:50 +0000

    Delete un-neccessary forward slash in url path

--- poems/urls.py
+++ poems/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.view_poems, name="poems"),
-    path("/edit_comment/<int:comment_id>",
+    path("edit_comment/<int:comment_id>",
          views.comment_edit, name='comment_edit'),
-    path("/delete_comment/<int:comment_id>",
+    path("delete_comment/<int:comment_id>",
          views.comment_delete, name='comment_delete'),
 ]

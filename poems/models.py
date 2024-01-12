ce2e39b0600e4d61d2d06731769be89773dce210
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-12 10:30:45 +0000

    Clean up comment model

--- poems/models.py
+++ poems/models.py
@@ -15,10 +15,8 @@ class Poem(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)

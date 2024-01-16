c19865baaa95c5c4a01cefe6e17973e43a452c6e
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-16 11:16:56 +0000

    Delete path view comments

--- poems/urls.py
+++ poems/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.view_poems, name="poems"),
-    path("", views.view_comments, name="comments"),
     path("edit_comment/<int:comment_id>",
          views.comment_edit, name='comment_edit'),
     path("delete_comment/<int:comment_id>",

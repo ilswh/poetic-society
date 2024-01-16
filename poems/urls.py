7d056417be6cd75d2b7c29df6a514c2be2267811
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-16 10:59:06 +0000

    Add url path to view comments

--- poems/urls.py
+++ poems/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.view_poems, name="poems"),
+    path("", views.view_comments, name="comments"),
     path("edit_comment/<int:comment_id>",
          views.comment_edit, name='comment_edit'),
     path("delete_comment/<int:comment_id>",

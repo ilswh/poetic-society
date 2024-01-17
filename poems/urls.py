1e35300d797e3ef53441ad92a461989134293f29
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-17 15:10:06 +0000

    Create new paths to add and view poems + beautify

--- poems/urls.py
+++ poems/urls.py
@@ -3,8 +3,20 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.view_poems, name="poems"),
-    path("edit_comment/<int:comment_id>",
-         views.comment_edit, name='comment_edit'),
-    path("delete_comment/<int:comment_id>",
-         views.comment_delete, name='comment_delete'),
+	path("add_poem/", views.add_poem, name="add_poem"),
+    path(
+		"view_poem/<int:poem_id>",
+		views.view_poem,
+		name='view_poem'
+	),
+    path(
+		"edit_comment/<int:comment_id>",
+		views.edit_comment,
+		name='comment_edit'
+	),
+    path(
+		"delete_comment/<int:comment_id>",
+		views.delete_comment,
+		name='comment_delete'
+	),
 ]

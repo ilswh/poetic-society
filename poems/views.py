0783285d268431cf1672f6a9a0d938e3b6e5a5a5
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-17 15:16:55 +0000

    Simplify code by deleting, change httpresponseredirect to return render, add imports, add definitions to add poems, view single poems

--- poems/views.py
+++ poems/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Poem, Comment
+from .forms import PoemForm
 
-# Create your views here.
 
 def view_poems(request):
+    """
+    A view to list all poems.
+    """
     poems = Poem.objects.all()
     template = "poems/poems.html"
     context = {
@@ -11,7 +15,41 @@ def view_poems(request):
     }
     return render(request, template, context)
 
+
+def view_poem(request, poem_id):
+    """
+    A view to display a poem.
+    """
+    poem = get_object_or_404(Poem, id=poem_id)
+    template = "poems/poem.html"
+    context = {
+        "poem": poem,
+    }
+    return render(request, template, context)
+
+
+def add_poem(request):
+    """
+    A view to add a new poem via the front-end.
+    """
+    form = PoemForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.instance.author = request.user
+            new_poem = form.save()
+            messages.success(request, "Poem added!")
+            return redirect(view_poem, poem_id=new_poem.pk)
+        messages.error(request, "Error: Please try again")
+    template = "poems/add_poem.html"
+    context = {
+        "form": form,
+    }
+    return render(request, template, context)
+
 def view_comments(request):
+    """
+    A view to display comments.
+    """
     comments = poem.comments.all().order_by("-created_on")
     comment_count = poem.comments.filter(approved=True).count()
     if request.method == "POST":
@@ -21,24 +59,25 @@ def view_comments(request):
             comment.author = request.user
             comment.poem = poem
             comment.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Comment submitted and awaiting approval'
+            messages.success(
+                request, 'Comment submitted and awaiting approval'
             )
-    
     comment_form = CommentForm()
 
-    return render(request,
-        {
-            "poem": poem,
-            "comments": comments,
-            "comment_count": comment_count,
-            "comment_form": comment_form
-        },
-    )
+    template = "poems/comments.html"
+    context = {
+        "poem": poem,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form
+    }
+    return render(request, template, context)
 
 
 def edit_comment(request, comment_id):
+    """
+    A view to edit a comment.
+    """
     if request.method == "POST":
 
         queryset = Post.objects.filter(status=1)
@@ -51,24 +90,25 @@ def edit_comment(request, comment_id):
             comment.poem = poem
             comment.approved = False
             comment.save()
-            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            messages.success(request, 'Comment Updated!')
         else:
-            messages.add_message(request, messages.ERROR,
-                                 'Error updating comment!')
+            messages.error(request, 'Error updating comment!')
 
-    return HttpResponseRedirect(reverse('poem_detail'))
+    return render(reverse('poem_detail'))
 
 
 def delete_comment(request, comment_id):
+    """
+    A view to delete a comment.
+    """
     queryset = Post.objects.filter(status=1)
     poem = get_object_or_404(queryset)
     comment = get_object_or_404(Comment, pk=comment_id)
 
     if comment.author == request.user:
         comment.delete()
-        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+        messages.success(request, 'Comment deleted!')
     else:
-        messages.add_message(request, messages.ERROR,
-                             'You can only delete your own comments!')
+        messages.error(request, 'You can only delete your own comments!')
 
-    return HttpResponseRedirect(reverse('poem_detail'))
+    return render(reverse('poem_detail'))

ff47cbb9db4825916da2b10999f30dff4e4a67b0
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-18 14:15:33 +0000

    Change debug to true

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['8000-ilswh-poeticsociety-944midrm5b7.ws-eu107.gitpod.io','.herokuapp.com']
 
